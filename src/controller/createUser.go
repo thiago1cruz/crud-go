7466c814c0867120ef5acee06f8560789dbfec04
Thiago araujo <thiago.tg.k2@hotmail.com>
2023-03-04 19:41:24 -0300

    feat: created response and request template

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,9 +1,16 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
+	"github.com/thiago1cruz/crud-go/src/controller/models/request"
 )
 
 func CreateUser(c *gin.Context) {
+
+	var userRequest request.UserRequest
+
+	fmt.Println(userRequest)
 
 }
